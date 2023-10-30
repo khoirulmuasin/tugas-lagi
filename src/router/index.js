2b4f546feeee3587227e4004fdd4b76deb344233
khoirulmuasin <gabutcrypto1@gmail.com>
2023-10-30 08:42:06 +0700

    belajar Tab navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react'
 import Home from '../pages/Home';
 import Splash from '../pages/Splash';
+import Akun from '../pages/Akun';
+import Pesanan from '../pages/Pesanan';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Pesanan" component={Pesanan} />
+        <Tab.Screen name="Akun" component={Akun} />
+      </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
-    <Stack.Navigator initialRouteName='Splash'>
+    <Stack.Navigator initialRouteName='MainApp'>
         <Stack.Screen name="Splash" component={Splash} options={{ headerShown:  false }} />
-        <Stack.Screen name="Home" component={Home} options={{ headerShown:  false }} />
+        <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown:  false }} />
     </Stack.Navigator>
   )
 }
